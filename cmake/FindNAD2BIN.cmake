7dde892c14a53d85e59f9ba7822197c7bdb9cc2d
Kristian Evers <kristianevers@gmail.com>
2019-02-04 22:03:15 +0100

    Add nad2bin
    PROJ 6 will not include the nad2bin application. Instead
    it is included here. CMake configuration is modified accordingly
    so that nad2bin is compiled and run as part of the packaging
    of the main datumgrid package.

--- cmake/FindNAD2BIN.cmake
+++
@@ -1,9 +0,0 @@
-# Find nad2bin executable required to convert .lla files
-
-find_program(NAD2BIN_EXECUTABLE nad2bin
-             PATHS /usr/local/bin
-                   /usr/bin)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(NAD2BIN DEFAULT_MSG NAD2BIN_EXECUTABLE)
-
